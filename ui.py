f118dc528d5dce6f8cf524de576db0828435397c
Pranav Pardeshi <im.pranav.pardeshi@gmail.com>
2026-03-29 14:54:56 +0530

    Added exception handling for bad response

--- ui.py
+++ ui.py
@@ -8,11 +8,15 @@ caste = st.selectbox("Select category", ["GOPENS", "GSCS", "GSTS", "GOBCS", "GSE
 branch = st.selectbox("Enter preferred branch: ", ['Artificial Intelligence and Data Science', 'Artificial Intelligence and Machine Learning', 'Civil Engineering', 'Computer Engineering', 'Computer Engineering (Software Engineering)', 'Computer Science and Engineering', 'Computer Science and Engineering (Artificial Intelligence)', 'Computer Science and Engineering (Internet of Things and Cyber Security Including Block Chain', 'Computer Science and Engineering(Artificial Intelligence and Machine Learning)', 'Computer Science and Engineering(Data Science)', 'Electrical Engineering', 'Electronics and Computer Engineering', 'Electronics and Telecommunication Engg', 'Information Technology', 'Instrumentation and Control Engineering', 'Manufacturing Science and Engineering', 'Mechanical Engineering', 'Metallurgy and Material Technology', 'Printing and Packing Technology'])
 
 if st.button("Find Colleges"):
-    response = requests.get("https://core-5y5r.onrender.com/recommender", params={
-        "percentile": percentile,
-        "caste": caste,
-        "branch": branch
-    })
+    try:
+        response = requests.get("https://core-5y5r.onrender.com/recommender", params={
+            "percentile": percentile,
+            "caste": caste,
+            "branch": branch
+        })
+    except Exception as e:
+        st.error("Server is waking up, Please try again in few minutes.")
+        st.stop()
 
     data = response.json()
 
