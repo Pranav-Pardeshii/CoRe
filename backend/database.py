d1c60f50261bfbca2f32db19f01ba1e94f033f1f
Pranav Pardeshi <im.pranav.pardeshi@gmail.com>
2026-04-13 03:39:45 +0530

    refactor: create seperate file for database

---
+++ backend/database.py
@@ -0,0 +1,15 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def get_db():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        port=int(os.getenv("DB_PORT")),
+        passwd=os.getenv("DB_PASS"),
+        database=os.getenv("DB_NAME"),
+        use_pure=True,
+    )
