5db867691ea1694439dc0f021258b9d8ea2da889
Pranav Pardeshi <im.pranav.pardeshi@gmail.com>
2026-07-12 11:55:28 +0530

    feat: add ping endpoint to keep the service awake

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from backend.routes import recommender, auth
+from backend.database import get_db
 
 
 app = FastAPI(title="CoRe - MHT-CET College Predictor")
@@ -10,3 +11,10 @@ app.include_router(auth.router)
 @app.get("/")
 def home():
     return {"message": "MHT-CET College Predictor"}
+
+@app.get("/ping")
+def ping(db = Depends(get_db)):
+    with db.cursor() as cursor:
+        cursor.execute("SELECT 1;")
+        cursor.fetchall()
+    return {"status": "Aiven MySQL Database is awake!"}
